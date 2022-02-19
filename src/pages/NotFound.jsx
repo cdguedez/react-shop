4dd985ffd90094c0d38767b92c1bef1f993d4041
cdguedez <cdgr.ing@gmail.com>
2022-02-18 21:45:48 -0400

    feat: Apply desing to pages not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Error404 from './../assets/logos/error-404.svg'
+import '../assets/styles/not-found.scss'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section className='error'>
+      <p className='error-title'>Wooops!! 👀 this page not found.</p>
+      <img className='error-img' src={Error404} alt="error 404" />
+      <div className='error-btn'>
+        <Link to={'/'}>Back to home</Link>
+      </div>
+    </section>
   )
 }
 
