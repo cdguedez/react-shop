5bc2eebbc749b677d31217fd708efca9046a0f29
cdguedez <cdgr.ing@gmail.com>
2022-02-22 17:14:26 -0400

    refactor: adjust Page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,154 +1,148 @@
-/**
- * Class Six
- */
-
-import React from "react"
-import "./../assets/styles/home.scss"
-import Header from "./../components/Header"
-import BtnCart from "./../assets/icons/bt_add_to_cart.svg"
-import ProductDetails from "../components/ProductDetails"
-import DesktopMenu from "../components/DesktopMenu"
+import React from 'react'
+import './../assets/styles/home.scss'
+import Header from './../components/Header'
+import BtnCart from './../assets/icons/bt_add_to_cart.svg'
+import ProductDetails from '../components/ProductDetails'
 
 const Home = () => {
   return (
     <>
       <Header />
-      <DesktopMenu />
       <ProductDetails />
-      <section className="main-container">
-        <div className="cards-container">
-          <div className="product-card">
+      <section className='main-container'>
+        <div className='cards-container'>
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
-          <div className="product-card">
+          <div className='product-card'>
             <img
-              src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt
+              src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='img-product'
             />
-            <div className="product-info">
+            <div className='product-info'>
               <div>
                 <p>$120,00</p>
                 <p>Bike</p>
               </div>
               <figure>
-                <img src={BtnCart} alt />
+                <img src={BtnCart} alt='btn-cart' />
               </figure>
             </div>
           </div>
